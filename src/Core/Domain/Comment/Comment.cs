200c9031504c6fef2fff80c5e1efa101680f5add
Nardos Wehabe <birsnot@gmail.com>
2023-08-22 07:49:17 -0300

    Adding the comment schema

---
+++ src/Core/Domain/Comment/Comment.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Common.Models;
+
+namespace Domain.Comment
+{
+    public class Comment : EntityBase
+    {
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+        public required string Content { get; set; }
+    }
+}
